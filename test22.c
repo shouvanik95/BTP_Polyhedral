1908b5e466fd4c35ae15fe17d68d3f27c172df55
Shouvanik <shouvanik.chakrabarti@gmail.com>
2016-11-27 15:43:24 +0530

    more tests

---
+++ test22.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int a[10][10];
+	int b[10][10];
+	int c[10][10];
+
+	#pragma scop
+
+	for(int i=1; i<10; i++) {
+		for(int j=0; j<10; j++){
+			b[i][j]=b[i-1][j+1];
+			a[i][j] = a[i-1][j-1];
+			c[i][j] = c[i+1][j-1];
+		}
+	}
+
+	#pragma endscop
+
+}
